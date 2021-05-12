c94b0f3548f5abff5c14f4c66a1ca2230837dd00
David Pitchford <david.t.pitchford@gmail.com>
2021-05-12 11:19:43 -0500

    Added guild name to logger messages where available

--- role/__init__.py
+++ role/__init__.py
@@ -2,13 +2,29 @@ import logging
 import logging.config
 import os
 
+class GuildNameFilter(logging.Filter):
+    def filter(self, record):
+        if hasattr(record, 'guild'):
+            record.guildname = f' ({record.guild.name})'
+        else:
+            record.guildname = ''
+        return True
+
 LOGGING_CONFIG = {
     'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
         'default': {
             'format': '%(asctime)s %(name)s %(filename)s.%(lineno)d %(levelname)s: %(message)s'
+        },
+        'discord': {
+            'format': '%(asctime)s %(name)s %(filename)s.%(lineno)d %(levelname)s%(guildname)s: %(message)s'
         }
+    },
+    'filters': {
+        'add_guild_name': {
+            '()': GuildNameFilter
+        },
     },
     'handlers': {
         'stream': {
@@ -16,13 +32,20 @@ LOGGING_CONFIG = {
             'level': logging.INFO,
             'formatter': 'default',
             'stream': 'ext://sys.stdout'
+        },
+        'discord_stream': {
+            'class': 'logging.StreamHandler',
+            'level': logging.INFO,
+            'formatter': 'discord',
+            'stream': 'ext://sys.stdout'
         }
     },
     'loggers': {
-        'discord': {
+        'role_bot': {
             'level': logging.INFO,
             'propagate': False,
-            'handlers': ['stream']
+            'handlers': ['discord_stream'],
+            'filters': ['add_guild_name']
         }
     },
     'root': {
@@ -40,8 +63,10 @@ if 'LOGFILE' in os.environ:
         'maxBytes': 2 ** 20,
         'backupCount': 10,
     }
+    LOGGING_CONFIG['handlers']['file_discord'] = LOGGING_CONFIG['handlers']['file'].copy()
+    LOGGING_CONFIG['handlers']['file_discord']['formatter'] = 'discord'
     LOGGING_CONFIG['root']['handlers'].append('file')
-    LOGGING_CONFIG['loggers']['discord']['handlers'].append('file')
+    LOGGING_CONFIG['loggers']['role_bot']['handlers'].append('file_discord')
 
 logging.config.dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger('role_bot')
--- role/role_reaction_bot.py
+++ role/role_reaction_bot.py
@@ -44,19 +44,20 @@ def parse_emoji(message):
             role = id_mapping[m.group(1)]
             emoji = line.replace(m.group(0), '').strip()
             if emoji not in ENGLISH_EMOJI:
-                logger.warning('Invalid emoji for role %s: %s', role.name, emoji)
+                logger.warning('Invalid emoji for role %s: %s', role.name, emoji,
+                    extra={'guild': guild})
                 continue
             emoji_to_role_mapping[emoji] = role
 
     logger.info('Parsed %d emoji/role pairs from message by @%s',
-        len(emoji_to_role_mapping), message.author.name)
-    logger.debug(str(dict(emoji_to_role_mapping)))
+        len(emoji_to_role_mapping), message.author.name, extra={'guild': guild})
+    logger.debug(str(dict(emoji_to_role_mapping)), extra={'guild': guild})
 
     return emoji_to_role_mapping
 
 async def update_message_reactions(message):
     logger.info('Updating reactions for message by @%s in #%s', message.author.name,
-        message.channel.name)
+        message.channel.name, extra={'guild': message.guild})
     await message.clear_reactions()
     # Scan through the message to find all emoji used
     emoji_to_role_mapping = parse_emoji(message)
@@ -74,20 +75,20 @@ class RoleReactionClient(commands.Bot):
 
     async def on_message(self, message):
         await super().on_message(message)
-        logger.debug('ON_MESSAGE')
+        logger.debug('ON_MESSAGE', extra={'guild': message.guild})
         if channel_match(message.channel):
             await update_message_reactions(message)
 
     async def on_raw_message_edit(self, payload):
-        logger.debug('ON_RAW_MESSAGE_EDIT')
         channel = self.get_channel(payload.channel_id)
+        logger.debug('ON_RAW_MESSAGE_EDIT', extra={'guild': channel.guild})
         if channel_match(channel):
             message = await channel.fetch_message(payload.message_id)
             await update_message_reactions(message)
 
     async def on_raw_reaction_add(self, payload):
-        logger.debug('ON_RAW_REACTION_ADD')
         channel = self.get_channel(payload.channel_id)
+        logger.debug('ON_RAW_REACTION_ADD', extra={'guild': channel.guild})
         if channel_match(channel):
             member = payload.member
             if member == self.user:
@@ -101,10 +102,12 @@ class RoleReactionClient(commands.Bot):
                 role = emoji_to_role_mapping[emoji.name]
 
                 if role in member.roles:
-                    logger.info('Removing role @%s from @%s', role.name, member.name)
+                    logger.info('Removing role @%s from @%s', role.name, member.name,
+                        extra={'guild': channel.guild})
                     await member.remove_roles(role, reason='Role bot')
                 else:
-                    logger.info('Adding role @%s to @%s', role.name, member.name)
+                    logger.info('Adding role @%s to @%s', role.name, member.name,
+                        extra={'guild': channel.guild})
                     await member.add_roles(role, reason='Role bot')
             else:
                 logger.warning('Unrecognized reaction: %s', emoji)
@@ -112,8 +115,8 @@ class RoleReactionClient(commands.Bot):
             await message.remove_reaction(emoji, member)
 
     async def on_raw_reaction_remove(self, payload):
-        logger.debug('ON_RAW_REACTION_REMOVE')
         channel = self.get_channel(payload.channel_id)
+        logger.debug('ON_RAW_REACTION_REMOVE', extra={'guild': channel.guild})
         if channel_match(channel):
             guild = self.get_guild(payload.guild_id)
             member = guild.get_member(payload.user_id)
