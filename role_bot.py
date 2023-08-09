15acc9c56855f70982f0d5457fa536844a4d1fc9
David Pitchford <david.t.pitchford@gmail.com>
2023-08-08 20:08:08 -0500

    Add dev mode

--- role_bot.py
+++ role_bot.py
@@ -1,6 +1,19 @@
 import os
+import argparse
 
 from role.role_reaction_bot import client
 import role.commands
 
-client.run(os.environ['DISCORD_TOKEN'])
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dev', action='store_true')
+    args = parser.parse_args()
+
+    if args.dev:
+        token = os.environ['DISCORD_DEV_TOKEN']
+    else:
+        token = os.environ['DISCORD_TOKEN']
+    client.run(token)
+
+if __name__ == '__main__':
+    main()
