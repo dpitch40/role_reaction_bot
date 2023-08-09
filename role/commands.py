b08d31a42b62ba674add5465a43fce58c0220d18
David Pitchford <david.t.pitchford@gmail.com>
2023-08-08 20:10:26 -0500

    Switch to slash commands for changing and creating roles

--- role/commands.py
+++ role/commands.py
@@ -1,39 +1,128 @@
+import itertools
+
+import discord
+
 from .role_reaction_bot import client, DEFAULT_CHANNEL_NAME
 
-@client.command()
-async def inrole(ctx, *args):
-    role_name = ' '.join(args)
-    role_name_lower = role_name.lower()
+def parse_color_string(s):
+    s = s.strip('#').lower()
+    if len(s) != 6:
+        raise ValueError("Invalid color string format")
+    r_hex = s[:2]
+    g_hex = s[2:4]
+    b_hex = s[4:]
+    try:
+        r = int(r_hex, 16)
+    except ValueError:
+        raise ValueError("Invalid color string format")
+    try:
+        g = int(g_hex, 16)
+    except ValueError:
+        raise ValueError("Invalid color string format")
+    try:
+        b = int(b_hex, 16)
+    except ValueError:
+        raise ValueError("Invalid color string format")
+    return r, g, b
+
+@client.slash_command(description="Check who has a pingable role")
+async def inrole(ctx,
+    role: discord.Option(
+        discord.SlashCommandOptionType.role,
+        description="The role to check",
+        required=True),
+):
+    members = role.members
+    joined_names = '\n'.join(sorted([f'> {member.name}' for member in members]))
+    message = f'{len(members)} members have role @{role.name}:\n{joined_names}'
+    await ctx.respond(message)
+
+# @client.command()
+# async def get_roles_channel(ctx):
+#     channel_name = client.role_channels.get(ctx.guild.name, DEFAULT_CHANNEL_NAME)
+#     for channel in ctx.guild.channels:
+#         if channel.name == channel_name:
+#             await ctx.send(f'The current roles channel being watched is {channel.mention}')
+#             break
+#     else:
+#         await ctx.send(f'The current roles channel being watched is {channel_name} (not found)')
+
+# @client.command()
+# async def set_roles_channel(ctx, new_name):
+#     if not ctx.author.permissions_in(ctx.message.channel).administrator:
+#         await ctx.send(f"I'm sorry {ctx.author.display_name}, I'm afraid I can't do that. (Because you aren't admin)")
+#         return
+#     channel_name = client.role_channels.get(ctx.guild.name, DEFAULT_CHANNEL_NAME)
+#     for channel in ctx.guild.channels:
+#         if channel.name == new_name:
+#             client.update_channel_mapping(ctx.guild, new_name)
+#             await ctx.send(f'Set roles channel to {channel.mention}')
+#             break
+#     else:
+#         await ctx.send(f'Channel named {new_name} not found')
+
+@client.application_command(description="Create a new pingable role")
+async def create_role(ctx,
+    name: discord.Option(
+        discord.SlashCommandOptionType.string,
+        description="The name of the role to add",
+        required=True),
+    character: discord.Option(
+        discord.SlashCommandOptionType.string,
+        description="Character(s) the role pertains to",
+        required=True),
+    color: discord.Option(
+        discord.SlashCommandOptionType.string,
+        description="Role color, in #xxxxxx format",
+        required=True),
+    description: discord.Option(
+        discord.SlashCommandOptionType.string,
+        description="Additional description of the role",
+        required=False)
+):
     for role in ctx.guild.roles:
-        if role.name.lower() == role_name_lower:
-            members = role.members
-            joined_names = '\n'.join(sorted([f'> {member.name}' for member in members]))
-            message = f'{len(members)} members have role @{role.name}:\n{joined_names}'
-            await ctx.send(message)
+        if role.name == name:
+            await ctx.respond(f"Role @{role.name} already exists")
             break
     else:
-        await ctx.send(f'Role {role_name!r} not found')
+        # Create role
+        try:
+            r, g, b = parse_color_string(color)
+        except ValueError as exc:
+            return await ctx.respond(str(exc))
 
-@client.command()
-async def get_roles_channel(ctx):
-    channel_name = client.role_channels.get(ctx.guild.name, DEFAULT_CHANNEL_NAME)
-    for channel in ctx.guild.channels:
-        if channel.name == channel_name:
-            await ctx.send(f'The current roles channel being watched is {channel.mention}')
-            break
-    else:
-        await ctx.send(f'The current roles channel being watched is {channel_name} (not found)')
+        color = discord.Colour.from_rgb(r, g, b)
+        role = await ctx.guild.create_role(name=name, colour=color, hoist=True, mentionable=True)
+        await ctx.respond(f"Created role {role.mention}!")
 
-@client.command()
-async def set_roles_channel(ctx, new_name):
-    if not ctx.author.permissions_in(ctx.message.channel).administrator:
-        await ctx.send(f"I'm sorry {ctx.author.display_name}, I'm afraid I can't do that. (Because you aren't admin)")
-        return
-    channel_name = client.role_channels.get(ctx.guild.name, DEFAULT_CHANNEL_NAME)
-    for channel in ctx.guild.channels:
-        if channel.name == new_name:
-            client.update_channel_mapping(ctx.guild, new_name)
-            await ctx.send(f'Set roles channel to {channel.mention}')
-            break
-    else:
-        await ctx.send(f'Channel named {new_name} not found')
+@client.slash_command(description="Give yourself a pingable role")
+async def apply_role(ctx,
+    role: discord.Option(
+        discord.SlashCommandOptionType.role,
+        description="The role to add",
+        required=True),
+):
+    if role.is_bot_managed():
+        return await ctx.respond("You can't fool me, you aren't a bot")
+    if role.is_premium_subscriber():
+        return await ctx.respond("Nice try, cheapskate")
+    if role.is_integration() or not role.is_assignable():
+        return await ctx.respond("Can't apply this role")
+    await ctx.author.add_roles(role)
+    await ctx.respond(f"Added you to {role.name}")
+
+@client.slash_command(description="Remove a pingable role from yourself")
+async def remove_role(ctx,
+    role: discord.Option(
+        discord.SlashCommandOptionType.role,
+        description="The role to remove",
+        required=True),
+):
+    if role.is_bot_managed():
+        return await ctx.respond("You can't fool me, you aren't a bot")
+    if role.is_premium_subscriber():
+        return await ctx.respond("Nice try, cheapskate")
+    if role.is_integration() or not role.is_assignable():
+        return await ctx.respond("Can't apply this role")
+    await ctx.author.remove_roles(role)
+    await ctx.respond(f"Removed you from {role.name}")
--- role/role_reaction_bot.py
+++ role/role_reaction_bot.py
@@ -31,39 +31,39 @@ DEFAULT_CHANNEL_NAME = 'roles'
 ENGLISH_EMOJI = load_emoji()
 CHANNEL_FILE = os.getenv('CHANNEL_FILE', 'role_channels.json')
 
-def parse_emoji(message):
-    guild = message.guild
-    roles = [guild.get_role(role_id) for role_id in message.raw_role_mentions]
-    id_mapping = {str(role.id): role for role in roles}
-    role_re = re.compile(f"<@&({'|'.join(i for i in id_mapping)})>")
+# def parse_emoji(message):
+#     guild = message.guild
+#     roles = [guild.get_role(role_id) for role_id in message.raw_role_mentions]
+#     id_mapping = {str(role.id): role for role in roles}
+#     role_re = re.compile(f"<@&({'|'.join(i for i in id_mapping)})>")
 
-    emoji_to_role_mapping = collections.OrderedDict()
-    for line in message.content.strip().split('\n'):
-        line = line.strip()
-        m = role_re.search(line)
-        if m:
-            role = id_mapping[m.group(1)]
-            emoji = line.replace(m.group(0), '').strip()
-            if emoji not in ENGLISH_EMOJI:
-                logger.warning('Invalid emoji for role %s: %s', role.name, emoji,
-                    extra={'guild': guild})
-                continue
-            emoji_to_role_mapping[emoji] = role
+#     emoji_to_role_mapping = collections.OrderedDict()
+#     for line in message.content.strip().split('\n'):
+#         line = line.strip()
+#         m = role_re.search(line)
+#         if m:
+#             role = id_mapping[m.group(1)]
+#             emoji = line.replace(m.group(0), '').strip()
+#             if emoji not in ENGLISH_EMOJI:
+#                 logger.warning('Invalid emoji for role %s: %s', role.name, emoji,
+#                     extra={'guild': guild})
+#                 continue
+#             emoji_to_role_mapping[emoji] = role
 
-    logger.info('Parsed %d emoji/role pairs from message by @%s',
-        len(emoji_to_role_mapping), message.author.name, extra={'guild': guild})
-    logger.debug(str(dict(emoji_to_role_mapping)), extra={'guild': guild})
+#     logger.info('Parsed %d emoji/role pairs from message by @%s',
+#         len(emoji_to_role_mapping), message.author.name, extra={'guild': guild})
+#     logger.debug(str(dict(emoji_to_role_mapping)), extra={'guild': guild})
 
-    return emoji_to_role_mapping
+#     return emoji_to_role_mapping
 
-async def update_message_reactions(message):
-    logger.info('Updating reactions for message by @%s in #%s', message.author.name,
-        message.channel.name, extra={'guild': message.guild})
-    await message.clear_reactions()
-    # Scan through the message to find all emoji used
-    emoji_to_role_mapping = parse_emoji(message)
-    for emoji in emoji_to_role_mapping.keys():
-        await message.add_reaction(emoji)
+# async def update_message_reactions(message):
+#     logger.info('Updating reactions for message by @%s in #%s', message.author.name,
+#         message.channel.name, extra={'guild': message.guild})
+#     await message.clear_reactions()
+#     # Scan through the message to find all emoji used
+#     emoji_to_role_mapping = parse_emoji(message)
+#     for emoji in emoji_to_role_mapping.keys():
+#         await message.add_reaction(emoji)
 
 # Event handlers
 
@@ -73,85 +73,85 @@ class RoleReactionClient(commands.Bot):
         self.channel_file = CHANNEL_FILE
         self.get_channel_mapping()
 
-    def get_channel_mapping(self):
-        if not os.path.isfile(self.channel_file):
-            self.role_channels = dict()
-            logger.info('Channel file not found at %s', self.channel_file)
-        else:
-            with open(self.channel_file, 'r') as fobj:
-                self.role_channels = json.load(fobj)
-                logger.info('Loaded %d role channels from %s', len(self.role_channels), self.channel_file)
+    # def get_channel_mapping(self):
+    #     if not os.path.isfile(self.channel_file):
+    #         self.role_channels = dict()
+    #         logger.info('Channel file not found at %s', self.channel_file)
+    #     else:
+    #         with open(self.channel_file, 'r') as fobj:
+    #             self.role_channels = json.load(fobj)
+    #             logger.info('Loaded %d role channels from %s', len(self.role_channels), self.channel_file)
 
-    def update_channel_mapping(self, guild, channel_name):
-        if channel_name == DEFAULT_CHANNEL_NAME:
-            logger.info('Resetting role channel for %s to default', guild.name)
-            if guild.name in self.role_channels:
-                del self.role_channels[guild.name]
-        else:
-            logger.info('Setting role channel for %s to %s', guild.name, channel_name)
-            self.role_channels[guild.name] = channel_name
-        with open(self.channel_file, 'w') as fobj:
-            json.dump(self.role_channels, fobj, indent=2)
-            logger.info('Saved %d role channels to %s', len(self.role_channels), self.channel_file)
+    # def update_channel_mapping(self, guild, channel_name):
+    #     if channel_name == DEFAULT_CHANNEL_NAME:
+    #         logger.info('Resetting role channel for %s to default', guild.name)
+    #         if guild.name in self.role_channels:
+    #             del self.role_channels[guild.name]
+    #     else:
+    #         logger.info('Setting role channel for %s to %s', guild.name, channel_name)
+    #         self.role_channels[guild.name] = channel_name
+    #     with open(self.channel_file, 'w') as fobj:
+    #         json.dump(self.role_channels, fobj, indent=2)
+    #         logger.info('Saved %d role channels to %s', len(self.role_channels), self.channel_file)
 
-    def channel_match(self, channel):
-        return channel.name == self.role_channels.get(channel.guild.name, DEFAULT_CHANNEL_NAME)
+    # def channel_match(self, channel):
+    #     return channel.name == self.role_channels.get(channel.guild.name, DEFAULT_CHANNEL_NAME)
 
     async def on_ready(self):
         logger.info('Logged in as %s', self.user)
 
-    async def on_message(self, message):
-        await super().on_message(message)
-        logger.debug('ON_MESSAGE', extra={'guild': message.guild})
-        if self.channel_match(message.channel):
-            await update_message_reactions(message)
+    # async def on_message(self, message):
+    #     await super().on_message(message)
+    #     logger.debug('ON_MESSAGE', extra={'guild': message.guild})
+    #     if self.channel_match(message.channel):
+    #         await update_message_reactions(message)
 
-    async def on_raw_message_edit(self, payload):
-        channel = self.get_channel(payload.channel_id)
-        logger.debug('ON_RAW_MESSAGE_EDIT', extra={'guild': channel.guild})
-        if self.channel_match(channel):
-            message = await channel.fetch_message(payload.message_id)
-            await update_message_reactions(message)
+    # async def on_raw_message_edit(self, payload):
+    #     channel = self.get_channel(payload.channel_id)
+    #     logger.debug('ON_RAW_MESSAGE_EDIT', extra={'guild': channel.guild})
+    #     if self.channel_match(channel):
+    #         message = await channel.fetch_message(payload.message_id)
+    #         await update_message_reactions(message)
 
-    async def on_raw_reaction_add(self, payload):
-        channel = self.get_channel(payload.channel_id)
-        logger.debug('ON_RAW_REACTION_ADD', extra={'guild': channel.guild})
-        if self.channel_match(channel):
-            member = payload.member
-            if member == self.user:
-                return
+    # async def on_raw_reaction_add(self, payload):
+    #     channel = self.get_channel(payload.channel_id)
+    #     logger.debug('ON_RAW_REACTION_ADD', extra={'guild': channel.guild})
+    #     if self.channel_match(channel):
+    #         member = payload.member
+    #         if member == self.user:
+    #             return
 
-            message = await channel.fetch_message(payload.message_id)
-            emoji = payload.emoji
+    #         message = await channel.fetch_message(payload.message_id)
+    #         emoji = payload.emoji
 
-            emoji_to_role_mapping = parse_emoji(message)
-            if emoji.name in emoji_to_role_mapping:
-                role = emoji_to_role_mapping[emoji.name]
+    #         emoji_to_role_mapping = parse_emoji(message)
+    #         if emoji.name in emoji_to_role_mapping:
+    #             role = emoji_to_role_mapping[emoji.name]
 
-                if role in member.roles:
-                    logger.info('Removing role @%s from @%s', role.name, member.name,
-                        extra={'guild': channel.guild})
-                    await member.remove_roles(role, reason='Role bot')
-                else:
-                    logger.info('Adding role @%s to @%s', role.name, member.name,
-                        extra={'guild': channel.guild})
-                    await member.add_roles(role, reason='Role bot')
-            else:
-                logger.warning('Unrecognized reaction: %s', emoji)
+    #             if role in member.roles:
+    #                 logger.info('Removing role @%s from @%s', role.name, member.name,
+    #                     extra={'guild': channel.guild})
+    #                 await member.remove_roles(role, reason='Role bot')
+    #             else:
+    #                 logger.info('Adding role @%s to @%s', role.name, member.name,
+    #                     extra={'guild': channel.guild})
+    #                 await member.add_roles(role, reason='Role bot')
+    #         else:
+    #             logger.warning('Unrecognized reaction: %s', emoji)
 
-            await message.remove_reaction(emoji, member)
+    #         await message.remove_reaction(emoji, member)
 
-    async def on_raw_reaction_remove(self, payload):
-        channel = self.get_channel(payload.channel_id)
-        logger.debug('ON_RAW_REACTION_REMOVE', extra={'guild': channel.guild})
-        if self.channel_match(channel):
-            guild = self.get_guild(payload.guild_id)
-            member = guild.get_member(payload.user_id)
-            if member != self.user:
-                return
+    # async def on_raw_reaction_remove(self, payload):
+    #     channel = self.get_channel(payload.channel_id)
+    #     logger.debug('ON_RAW_REACTION_REMOVE', extra={'guild': channel.guild})
+    #     if self.channel_match(channel):
+    #         guild = self.get_guild(payload.guild_id)
+    #         member = guild.get_member(payload.user_id)
+    #         if member != self.user:
+    #             return
 
-            message = await channel.fetch_message(payload.message_id)
-            await update_message_reactions(message)
+    #         message = await channel.fetch_message(payload.message_id)
+    #         await update_message_reactions(message)
 
     async def on_error(self, event, *args, **kwargs):
         logger.error('Some bug happened %s', event, exc_info=True)
